0ab9d273e52bae19df41b27feb1acc05204c67f7
Samuel Bediako <samuelbediako020.com>
2022-10-31 05:33:33 -0700

    second commit

--- index.js
+++ index.js
@@ -13,3 +13,17 @@ ham.addEventListener("click", () => {
     document.body.style.backgroundColor = "rgb(219, 219, 219)";
   }
 });
+
+//fetch api
+
+async function youtube() {
+  let response = await fetch(
+    "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=ajax&type=video&key=AIzaSyCk7xTmPnD7dEgi7j2S2d8xNeWCgi9CqCI"
+  );
+
+  let youtubedata = await response.json();
+
+  console.log(youtubedata);
+}
+
+youtube();
