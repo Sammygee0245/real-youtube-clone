d5febfdc0c085737e3f6199f4af40c6a6cb072e3
Samuel Bediako <samuelbediako020.com>
2022-10-31 12:40:08 -0700

    fourth commit

--- index.js
+++ index.js
@@ -6,11 +6,9 @@ ham.addEventListener("click", () => {
   if (ham.classList.contains("menu")) {
     ham.classList.remove("menu");
     nav_left.style.display = "none";
-    document.body.style.backgroundColor = "#fff";
   } else {
     ham.classList.add("menu");
     nav_left.style.display = "flex";
-    document.body.style.backgroundColor = "rgb(219, 219, 219)";
   }
 });
 
