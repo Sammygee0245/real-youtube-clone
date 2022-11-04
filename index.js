122fad29ba7cfa27495cfd7279faff30eeeb4580
Samuel Bediako <samuelbediako020.com>
2022-11-04 09:15:51 -0700

    Al

--- index.js
+++ index.js
@@ -100,6 +100,66 @@ mainsearch.addEventListener("click", () => {
       let chantitle12 = document.querySelector(".channeltitle-12");
       let frame12 = document.querySelector(".frame-12");
 
+      let videopre13 = document.querySelector(".video-div-13");
+      let videodet13 = document.querySelector(".title-13");
+      let chantitle13 = document.querySelector(".channeltitle-13");
+      let frame13 = document.querySelector(".frame-13");
+
+      let videopre14 = document.querySelector(".video-div-14");
+      let videodet14 = document.querySelector(".title-14");
+      let chantitle14 = document.querySelector(".channeltitle-14");
+      let frame14 = document.querySelector(".frame-14");
+
+      let videopre15 = document.querySelector(".video-div-15");
+      let videodet15 = document.querySelector(".title-15");
+      let chantitle15 = document.querySelector(".channeltitle-15");
+      let frame15 = document.querySelector(".frame-15");
+
+      let videopre16 = document.querySelector(".video-div-16");
+      let videodet16 = document.querySelector(".title-16");
+      let chantitle16 = document.querySelector(".channeltitle-16");
+      let frame16 = document.querySelector(".frame-16");
+
+      let videopre17 = document.querySelector(".video-div-17");
+      let videodet17 = document.querySelector(".title-17");
+      let chantitle17 = document.querySelector(".channeltitle-17");
+      let frame17 = document.querySelector(".frame-17");
+
+      let videopre18 = document.querySelector(".video-div-18");
+      let videodet18 = document.querySelector(".title-18");
+      let chantitle18 = document.querySelector(".channeltitle-18");
+      let frame18 = document.querySelector(".frame-18");
+
+      let videopre19 = document.querySelector(".video-div-19");
+      let videodet19 = document.querySelector(".title-19");
+      let chantitle19 = document.querySelector(".channeltitle-19");
+      let frame19 = document.querySelector(".frame-19");
+
+      let videopre20 = document.querySelector(".video-div-20");
+      let videodet20 = document.querySelector(".title-20");
+      let chantitle20 = document.querySelector(".channeltitle-20");
+      let frame20 = document.querySelector(".frame-20");
+
+      let videopre21 = document.querySelector(".video-div-21");
+      let videodet21 = document.querySelector(".title-21");
+      let chantitle21 = document.querySelector(".channeltitle-21");
+      let frame21 = document.querySelector(".frame-21");
+
+      let videopre22 = document.querySelector(".video-div-22");
+      let videodet22 = document.querySelector(".title-22");
+      let chantitle22 = document.querySelector(".channeltitle-22");
+      let frame22 = document.querySelector(".frame-22");
+
+      let videopre23 = document.querySelector(".video-div-23");
+      let videodet23 = document.querySelector(".title-23");
+      let chantitle23 = document.querySelector(".channeltitle-23");
+      let frame23 = document.querySelector(".frame-23");
+
+      let videopre24 = document.querySelector(".video-div-24");
+      let videodet24 = document.querySelector(".title-24");
+      let chantitle24 = document.querySelector(".channeltitle-24");
+      let frame24 = document.querySelector(".frame-24");
+
       let response = await fetch(
         "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=" +
           searchItem +
@@ -227,6 +287,102 @@ mainsearch.addEventListener("click", () => {
         "href",
         "https://www.youtube.com/watch?v=" + videoid[11]
       );
+
+      videopre13.setAttribute("src", videothumbnail[12].url);
+      videodet13.innerHTML = title[12];
+      chantitle13.innerHTML = chaneltitle[12];
+      frame13.setAttribute(
+        "href",
+        "https://www.youtube.com/watch?v=" + videoid[12]
+      );
+
+      videopre14.setAttribute("src", videothumbnail[13].url);
+      videodet14.innerHTML = title[13];
+      chantitle14.innerHTML = chaneltitle[13];
+      frame14.setAttribute(
+        "href",
+        "https://www.youtube.com/watch?v=" + videoid[13]
+      );
+
+      videopre15.setAttribute("src", videothumbnail[14].url);
+      videodet15.innerHTML = title[14];
+      chantitle15.innerHTML = chaneltitle[14];
+      frame15.setAttribute(
+        "href",
+        "https://www.youtube.com/watch?v=" + videoid[14]
+      );
+
+      videopre16.setAttribute("src", videothumbnail[15].url);
+      videodet16.innerHTML = title[15];
+      chantitle16.innerHTML = chaneltitle[15];
+      frame16.setAttribute(
+        "href",
+        "https://www.youtube.com/watch?v=" + videoid[15]
+      );
+
+      videopre17.setAttribute("src", videothumbnail[16].url);
+      videodet17.innerHTML = title[16];
+      chantitle17.innerHTML = chaneltitle[16];
+      frame17.setAttribute(
+        "href",
+        "https://www.youtube.com/watch?v=" + videoid[16]
+      );
+
+      videopre18.setAttribute("src", videothumbnail[17].url);
+      videodet18.innerHTML = title[17];
+      chantitle18.innerHTML = chaneltitle[17];
+      frame18.setAttribute(
+        "href",
+        "https://www.youtube.com/watch?v=" + videoid[17]
+      );
+
+      videopre19.setAttribute("src", videothumbnail[18].url);
+      videodet19.innerHTML = title[18];
+      chantitle19.innerHTML = chaneltitle[18];
+      frame19.setAttribute(
+        "href",
+        "https://www.youtube.com/watch?v=" + videoid[18]
+      );
+
+      videopre20.setAttribute("src", videothumbnail[19].url);
+      videodet20.innerHTML = title[19];
+      chantitle20.innerHTML = chaneltitle[19];
+      frame20.setAttribute(
+        "href",
+        "https://www.youtube.com/watch?v=" + videoid[19]
+      );
+
+      videopre21.setAttribute("src", videothumbnail[20].url);
+      videodet21.innerHTML = title[20];
+      chantitle21.innerHTML = chaneltitle[20];
+      frame21.setAttribute(
+        "href",
+        "https://www.youtube.com/watch?v=" + videoid[20]
+      );
+
+      videopre22.setAttribute("src", videothumbnail[21].url);
+      videodet22.innerHTML = title[21];
+      chantitle22.innerHTML = chaneltitle[21];
+      frame22.setAttribute(
+        "href",
+        "https://www.youtube.com/watch?v=" + videoid[21]
+      );
+
+      videopre23.setAttribute("src", videothumbnail[22].url);
+      videodet23.innerHTML = title[22];
+      chantitle23.innerHTML = chaneltitle[22];
+      frame23.setAttribute(
+        "href",
+        "https://www.youtube.com/watch?v=" + videoid[22]
+      );
+
+      videopre24.setAttribute("src", videothumbnail[23].url);
+      videodet24.innerHTML = title[23];
+      chantitle24.innerHTML = chaneltitle[23];
+      frame24.setAttribute(
+        "href",
+        "https://www.youtube.com/watch?v=" + videoid[23]
+      );
     }
 
     direct.style.display = "none";
@@ -296,6 +452,66 @@ async function youtube() {
   let videodet12 = document.querySelector(".title-12");
   let chantitle12 = document.querySelector(".channeltitle-12");
   let frame12 = document.querySelector(".frame-12");
+
+  let videopre13 = document.querySelector(".video-div-13");
+  let videodet13 = document.querySelector(".title-13");
+  let chantitle13 = document.querySelector(".channeltitle-13");
+  let frame13 = document.querySelector(".frame-13");
+
+  let videopre14 = document.querySelector(".video-div-14");
+  let videodet14 = document.querySelector(".title-14");
+  let chantitle14 = document.querySelector(".channeltitle-14");
+  let frame14 = document.querySelector(".frame-14");
+
+  let videopre15 = document.querySelector(".video-div-15");
+  let videodet15 = document.querySelector(".title-15");
+  let chantitle15 = document.querySelector(".channeltitle-15");
+  let frame15 = document.querySelector(".frame-15");
+
+  let videopre16 = document.querySelector(".video-div-16");
+  let videodet16 = document.querySelector(".title-16");
+  let chantitle16 = document.querySelector(".channeltitle-16");
+  let frame16 = document.querySelector(".frame-16");
+
+  let videopre17 = document.querySelector(".video-div-17");
+  let videodet17 = document.querySelector(".title-17");
+  let chantitle17 = document.querySelector(".channeltitle-17");
+  let frame17 = document.querySelector(".frame-17");
+
+  let videopre18 = document.querySelector(".video-div-18");
+  let videodet18 = document.querySelector(".title-18");
+  let chantitle18 = document.querySelector(".channeltitle-18");
+  let frame18 = document.querySelector(".frame-18");
+
+  let videopre19 = document.querySelector(".video-div-19");
+  let videodet19 = document.querySelector(".title-19");
+  let chantitle19 = document.querySelector(".channeltitle-19");
+  let frame19 = document.querySelector(".frame-19");
+
+  let videopre20 = document.querySelector(".video-div-20");
+  let videodet20 = document.querySelector(".title-20");
+  let chantitle20 = document.querySelector(".channeltitle-20");
+  let frame20 = document.querySelector(".frame-20");
+
+  let videopre21 = document.querySelector(".video-div-21");
+  let videodet21 = document.querySelector(".title-21");
+  let chantitle21 = document.querySelector(".channeltitle-21");
+  let frame21 = document.querySelector(".frame-21");
+
+  let videopre22 = document.querySelector(".video-div-22");
+  let videodet22 = document.querySelector(".title-22");
+  let chantitle22 = document.querySelector(".channeltitle-22");
+  let frame22 = document.querySelector(".frame-22");
+
+  let videopre23 = document.querySelector(".video-div-23");
+  let videodet23 = document.querySelector(".title-23");
+  let chantitle23 = document.querySelector(".channeltitle-23");
+  let frame23 = document.querySelector(".frame-23");
+
+  let videopre24 = document.querySelector(".video-div-24");
+  let videodet24 = document.querySelector(".title-24");
+  let chantitle24 = document.querySelector(".channeltitle-24");
+  let frame24 = document.querySelector(".frame-24");
 
   let response = await fetch(
     "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=cat&type=video&key=AIzaSyCk7xTmPnD7dEgi7j2S2d8xNeWCgi9CqCI&maxResults=25"
@@ -391,6 +607,102 @@ async function youtube() {
   frame12.setAttribute(
     "href",
     "https://www.youtube.com/watch?v=" + videoid[11]
+  );
+
+  videopre13.setAttribute("src", videothumbnail[12].url);
+  videodet13.innerHTML = title[12];
+  chantitle13.innerHTML = chaneltitle[12];
+  frame13.setAttribute(
+    "href",
+    "https://www.youtube.com/watch?v=" + videoid[12]
+  );
+
+  videopre14.setAttribute("src", videothumbnail[13].url);
+  videodet14.innerHTML = title[13];
+  chantitle14.innerHTML = chaneltitle[13];
+  frame14.setAttribute(
+    "href",
+    "https://www.youtube.com/watch?v=" + videoid[13]
+  );
+
+  videopre15.setAttribute("src", videothumbnail[14].url);
+  videodet15.innerHTML = title[14];
+  chantitle15.innerHTML = chaneltitle[14];
+  frame15.setAttribute(
+    "href",
+    "https://www.youtube.com/watch?v=" + videoid[14]
+  );
+
+  videopre16.setAttribute("src", videothumbnail[15].url);
+  videodet16.innerHTML = title[15];
+  chantitle16.innerHTML = chaneltitle[15];
+  frame16.setAttribute(
+    "href",
+    "https://www.youtube.com/watch?v=" + videoid[15]
+  );
+
+  videopre17.setAttribute("src", videothumbnail[16].url);
+  videodet17.innerHTML = title[16];
+  chantitle17.innerHTML = chaneltitle[16];
+  frame17.setAttribute(
+    "href",
+    "https://www.youtube.com/watch?v=" + videoid[16]
+  );
+
+  videopre18.setAttribute("src", videothumbnail[17].url);
+  videodet18.innerHTML = title[17];
+  chantitle18.innerHTML = chaneltitle[17];
+  frame18.setAttribute(
+    "href",
+    "https://www.youtube.com/watch?v=" + videoid[17]
+  );
+
+  videopre19.setAttribute("src", videothumbnail[18].url);
+  videodet19.innerHTML = title[18];
+  chantitle19.innerHTML = chaneltitle[18];
+  frame19.setAttribute(
+    "href",
+    "https://www.youtube.com/watch?v=" + videoid[18]
+  );
+
+  videopre20.setAttribute("src", videothumbnail[19].url);
+  videodet20.innerHTML = title[19];
+  chantitle20.innerHTML = chaneltitle[19];
+  frame20.setAttribute(
+    "href",
+    "https://www.youtube.com/watch?v=" + videoid[19]
+  );
+
+  videopre21.setAttribute("src", videothumbnail[20].url);
+  videodet21.innerHTML = title[20];
+  chantitle21.innerHTML = chaneltitle[20];
+  frame21.setAttribute(
+    "href",
+    "https://www.youtube.com/watch?v=" + videoid[20]
+  );
+
+  videopre22.setAttribute("src", videothumbnail[21].url);
+  videodet22.innerHTML = title[21];
+  chantitle22.innerHTML = chaneltitle[21];
+  frame22.setAttribute(
+    "href",
+    "https://www.youtube.com/watch?v=" + videoid[21]
+  );
+
+  videopre23.setAttribute("src", videothumbnail[22].url);
+  videodet23.innerHTML = title[22];
+  chantitle23.innerHTML = chaneltitle[22];
+  frame23.setAttribute(
+    "href",
+    "https://www.youtube.com/watch?v=" + videoid[22]
+  );
+
+  videopre24.setAttribute("src", videothumbnail[23].url);
+  videodet24.innerHTML = title[23];
+  chantitle24.innerHTML = chaneltitle[23];
+  frame24.setAttribute(
+    "href",
+    "https://www.youtube.com/watch?v=" + videoid[23]
   );
 
   console.log(chanelid);
