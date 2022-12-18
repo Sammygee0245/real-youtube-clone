5a40cf5bc20879b489684e97373dc3e90f3ce04b
Samuel Bediako <samuelbediako020.com>
2022-12-18 09:34:27 -0800

    search

--- index.js
+++ index.js
@@ -30,6 +30,43 @@ car.addEventListener("click", () => {
   console.log("existed");
 });
 
+mainsearch.addEventListener("click",()=>{
+  tube()
+})
+
+
+async function tube(){
+  let search = document.querySelector("#search-stuff").value
+  console.log(search)
+
+  let response = await fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&q="+search+"&type=video&key=AIzaSyCk7xTmPnD7dEgi7j2S2d8xNeWCgi9CqCI&maxResults=24")
+  let data = await response.json()
+  let vid = data.items
+  console.log(vid)
+
+
+  if(vid.length !== 0){
+    videoContainer.innerHTML = vid.map((x)=>{
+        let {snippet,id} = x
+        return ( `
+      <a href=${"https://www.youtube.com/watch?v="+id.videoId} class="video">
+            <img width="100%" height="50%" src=${snippet.thumbnails.medium.url} alt="">
+            <div class="video-details">
+                <img class="channel-pic" src=${snippet.thumbnails.medium.url} alt="">
+                <div class="vidoe-prev-info">
+                    <p class="vidTitile">${snippet.title}</p>
+                    <p class="channel-name">${snippet.channelTitle}</p>
+                </div>
+            </div>
+      </a>
+      `)
+    }).join("")
+    videoContainer.style.backgroundColor = "rgb(22, 22, 22)"
+    direct.style.display = "none"
+  }else{}
+}
+
+
 
 //fetch api
 
